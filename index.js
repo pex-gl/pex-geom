a6e3377208ece2761de2d3b2966866d7a3ee638e
Marcin Ignac <vorg@Makowka.local>
2014-04-15 23:27:40 +0100

    Initial code ported from pex v2

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports.Vec2 = require('./lib/Vec2');
+module.exports.Vec3 = require('./lib/Vec3');
+module.exports.Vec4 = require('./lib/Vec4');
+module.exports.Mat4 = require('./lib/Mat4');
+module.exports.Quat = require('./lib/Quat');
+module.exports.Ray = require('./lib/Ray');
+module.exports.Geometry = require('./lib/Geometry');
+module.exports.gen = {
+  Cube: require('./lib/gen/Cube'),
+  Sphere: require('./lib/gen/Sphere')
+}
