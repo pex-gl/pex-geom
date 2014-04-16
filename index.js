3b6cc17dcda3d759b688581d838f46853f2dbed5
Marcin Ignac <vorg@Makowka.local>
2014-04-16 17:20:48 +0100

    Added Path, Spline1D, Loft and dependency on merge

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ module.exports.Vec3 = require('./lib/Vec3');
 module.exports.Vec4 = require('./lib/Vec4');
 module.exports.Mat4 = require('./lib/Mat4');
 module.exports.Quat = require('./lib/Quat');
+module.exports.Path = require('./lib/Path');
 module.exports.Spline3D = require('./lib/Spline3D');
+module.exports.Spline1D = require('./lib/Spline1D');
 module.exports.Ray = require('./lib/Ray');
 module.exports.Geometry = require('./lib/Geometry');
 module.exports.gen = {
   Cube: require('./lib/gen/Cube'),
   Sphere: require('./lib/gen/Sphere'),
-  LineBuilder: require('./lib/gen/LineBuilder')
+  LineBuilder: require('./lib/gen/LineBuilder'),
+  Loft: require('./lib/gen/Loft')
 }
