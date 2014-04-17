f3bc009cc187c2fc48dea2a91dd8be961069948c
Marcin Ignac <vorg@Makowka.local>
2014-04-17 12:34:09 +0100

    Added Rect

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ module.exports.Vec4 = require('./lib/Vec4');
 module.exports.Mat4 = require('./lib/Mat4');
 module.exports.Quat = require('./lib/Quat');
 module.exports.Path = require('./lib/Path');
+module.exports.Rect = require('./lib/Rect');
 module.exports.Spline3D = require('./lib/Spline3D');
 module.exports.Spline1D = require('./lib/Spline1D');
 module.exports.Ray = require('./lib/Ray');
