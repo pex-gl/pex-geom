47729e7b8baf2beeae09e9b74ee03f1d17afa70a
Marcin Ignac <marcin.ignac@gmail.com>
2014-08-07 10:27:27 +0200

    Added Triangle3D

--- index.js
+++ index.js
@@ -5,15 +5,16 @@ module.exports.Mat4 = require('./lib/Mat4');
 module.exports.Quat = require('./lib/Quat');
 module.exports.Path = require('./lib/Path');
 module.exports.Rect = require('./lib/Rect');
-module.exports.Spline3D = require('./lib/Spline3D');
-module.exports.Spline2D = require('./lib/Spline2D');
-module.exports.Spline1D = require('./lib/Spline1D');
+module.exports.Spline3 = require('./lib/Spline3');
+module.exports.Spline2 = require('./lib/Spline2');
+module.exports.Spline1 = require('./lib/Spline1');
 module.exports.Ray = require('./lib/Ray');
 module.exports.Plane = require('./lib/Plane');
 module.exports.Geometry = require('./lib/Geometry');
 module.exports.Random = require('./lib/Random');
 module.exports.BoundingBox = require('./lib/BoundingBox');
-module.exports.Triangle2D = require('./lib/Triangle2D');
+module.exports.Triangle2 = require('./lib/Triangle2');
+module.exports.Triangle3 = require('./lib/Triangle3');
 module.exports.Octree = require('./lib/Octree');
 
 //unpack Random methods to geom package
