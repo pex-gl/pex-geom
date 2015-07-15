3811ca0d5a6769ed9b2fa72e833b66d7eb91738d
Henryk Wollik <olafyoorsen@hotmail.com>
2015-07-15 11:35:07 +0200

    Rect moved to min[],max[]

--- Rect.js
+++ Rect.js
@@ -1,78 +1,78 @@
 function create(){
-    return [0,0,0,0];
+    return [[0,0],[0,0]];
 }
 
 function copy(a){
-    return a.slice(0);
+    return [a[0].slice(0),a[1].slice()];
 }
 
 function set(a,b){
-    a[0] = b[0];
-    a[1] = b[1];
-    a[2] = b[2];
-    a[3] = b[3];
+    a[0][0] = b[0][0];
+    a[0][1] = b[0][1];
+    a[1][0] = b[1][0];
+    a[1][1] = b[1][1];
     return a;
 }
 
 function set4(a,x,y,w,h){
-    a[0] = x;
-    a[1] = y;
-    a[2] = x + w;
-    a[3] = y + h;
+    a[0][0] = x;
+    a[0][1] = y;
+    a[1][0] = x + w;
+    a[1][1] = y + h;
     return a;
 }
 
 function scale(a,n){
-    a[0] *= n;
-    a[1] *= n;
-    a[2] *= n;
-    a[3] *= n;
+    a[0][0] *= n;
+    a[0][1] *= n;
+    a[1][0] *= n;
+    a[1][1] *= n;
     return a;
 }
 
 function setMinMax(a,min,max){
-    a[0] = min[0];
-    a[1] = min[1];
-    a[2] = max[0];
-    a[3] = max[1];
+    a[0][0] = min[0];
+    a[0][1] = min[1];
+    a[1][0] = max[0];
+    a[1][1] = max[1];
     return a;
 }
 
 function setMinMax4(a,minx,miny,maxx,maxy){
-    a[0] = minx;
-    a[1] = miny;
-    a[2] = maxx;
-    a[3] = maxy;
+    a[0][0] = minx;
+    a[0][1] = miny;
+    a[1][0] = maxx;
+    a[1][1] = maxy;
     return a;
 }
 
 function getMin(a,out){
     if(out === undefined){
-        return [a[0],a[1]];
+        return [a[0][0],a[1]];
     }
-    out[0] = a[0];
-    out[1] = a[1];
+    out[0] = a[0][0];
+    out[1] = a[0][1];
     return out;
 }
 
 function getMax(a,out){
     if(out === undefined){
-        return [a[2],a[3]];
+        return [a[1][0],a[1][1]];
     }
-    out[0] = a[2];
-    out[1] = a[3];
+    out[0] = a[1][0];
+    out[1] = a[1][1];
     return out;
 }
 
 function setSize2(a,width,height){
-    a[2] = a[0] + width;
-    a[3] = a[1] + height;
+    a[1][0] = a[0][0] + width;
+    a[1][1] = a[0][1] + height;
     return a;
 }
 
 function setSize(a,size){
-    a[2] = a[0] + size[0];
-    a[3] = a[1] + size[1];
+    a[1][0] = a[0][0] + size[0];
+    a[1][1] = a[0][1] + size[1];
     return a;
 }
 
@@ -88,21 +88,21 @@ function getSize(a,out){
 }
 
 function setWidth(a,width){
-    a[2] = a[0] + width;
+    a[1][0] = a[0][0] + width;
     return a;
 }
 
 function setHeight(a,height){
-    a[3] = a[1] + height;
+    a[1][1] = a[0][1] + height;
     return a;
 }
 
 function getWidth(a){
-    return a[2] - a[0];
+    return a[1][0] - a[0][0];
 }
 
 function getHeight(a){
-    return a[3] - a[1];
+    return a[1][1] - a[0][1];
 }
 
 function getAspectRatio(a){
@@ -114,28 +114,28 @@ function setPosition(a,position){
 }
 
 function setPosition2(a,x,y){
-    a[0] = x;
-    a[1] = y;
-    a[2] = x + getWidth(a);
-    a[3] = y + getHeight(a);
+    a[0][0] = x;
+    a[0][1] = y;
+    a[1][0] = x + getWidth(a);
+    a[1][1] = y + getHeight(a);
     return a;
 }
 
 function getTR(a,out){
     if(out === undefined){
-        return [a[2],a[1]];
+        return [a[1][0],a[1]];
     }
-    out[0] = a[2];
-    out[1] = a[1];
+    out[0] = a[1][0];
+    out[1] = a[0][1];
     return out;
 }
 
 function getBL(a,out){
     if(out === undefined){
-        return [a[0],a[3]];
+        return [a[0][0],a[1][1]];
     }
-    out[0] = a[0];
-    out[1] = a[3];
+    out[0] = a[0][0];
+    out[1] = a[1][1];
     return out;
 }
 
@@ -155,11 +155,11 @@ function containsPoint(a,point){
 }
 
 function containsPoint2(a,x,y){
-    return x >= a[0] && x <= a[2] && y >= a[1] && y <= a[3];
+    return x >= a[0][0] && x <= a[1][0] && y >= a[0][1] && y <= a[1][1];
 }
 
 function containsRect(a,b){
-    return containsPoint2(a,b[0],b[1]) && containsPoint2(a,b[2],b[3]);
+    return containsPoint2(a,b[0][0],b[0][1]) && containsPoint2(a,b[1][0],b[1][1]);
 }
 
 function includePoint(a,point){
@@ -168,34 +168,34 @@ function includePoint(a,point){
 
 function includePoint2(a,x,y){
     if(isZero(a)){
-        a[0] = a[2] = x;
-        a[1] = a[3] = y;
+        a[0][0] = a[1][0] = x;
+        a[0][1] = a[1][1] = y;
         return a;
     }
 
-    var minx = a[0];
-    var miny = a[1];
-    var maxx = a[2];
-    var maxy = a[3];
+    var minx = a[0][0];
+    var miny = a[0][1];
+    var maxx = a[1][0];
+    var maxy = a[1][1];
 
-    a[0] = minx > x ? x : minx;
-    a[1] = miny > y ? y : miny;
-    a[2] = maxx < x ? x : maxx;
-    a[3] = maxy < y ? y : maxy;
+    a[0][0] = minx > x ? x : minx;
+    a[0][1] = miny > y ? y : miny;
+    a[1][0] = maxx < x ? x : maxx;
+    a[1][1] = maxy < y ? y : maxy;
 
     return a;
 }
 
 function includePoints(a,points){
     if(isZero(a)){
-        a[0] = a[1] =  Number.MAX_VALUE;
-        a[2] = a[3] = -Number.MAX_VALUE;
+        a[0][0] = a[0][1] =  Number.MAX_VALUE;
+        a[1][0] = a[1][1] = -Number.MAX_VALUE;
     }
 
-    var minx = a[0];
-    var miny = a[1];
-    var maxx = a[2];
-    var maxy = a[3];
+    var minx = a[0][0];
+    var miny = a[0][1];
+    var maxx = a[1][0];
+    var maxy = a[1][1];
 
     for(var i = 0, l = points.length, x, y; i < l; ++i){
         x = points[i][0];
@@ -207,23 +207,23 @@ function includePoints(a,points){
         maxy = maxy < y ? y : maxy;
     }
 
-    a[0] = minx;
-    a[1] = miny;
-    a[2] = maxx;
-    a[3] = maxy;
+    a[0][0] = minx;
+    a[0][1] = miny;
+    a[1][0] = maxx;
+    a[1][1] = maxy;
 
     return a;
 }
 
 function includePointsFlat(a,points){
     if(isZero(a)){
-        a[0] = a[1] =  Number.MAX_VALUE;
-        a[2] = a[3] = -Number.MAX_VALUE;
+        a[0][0] = a[0][1] =  Number.MAX_VALUE;
+        a[1][0] = a[1][1] = -Number.MAX_VALUE;
     }
-    var minx = a[0];
-    var miny = a[1];
-    var maxx = a[2];
-    var maxy = a[3];
+    var minx = a[0][0];
+    var miny = a[0][1];
+    var maxx = a[1][0];
+    var maxy = a[1][1];
 
     for(var i = 0, l = points.length, x, y; i < l; i+=2){
         x = points[i  ];
@@ -235,17 +235,17 @@ function includePointsFlat(a,points){
         maxy = maxy < y ? y : maxy;
     }
 
-    a[0] = minx;
-    a[1] = miny;
-    a[2] = maxx;
-    a[3] = maxy;
+    a[0][0] = minx;
+    a[0][1] = miny;
+    a[1][0] = maxx;
+    a[1][1] = maxy;
 
     return a;
 }
 
 function includeRect(a,rect){
-    includePoint2(a,rect[0],rect[1]);
-    includePoint2(a,rect[2],rect[3]);
+    includePoint(a,rect[0]);
+    includePoint(a,rect[1]);
     return a;
 }
 
@@ -257,17 +257,17 @@ function includeRects(a,rects){
 }
 
 function isZero(a){
-    return a[0] == 0 && a[1] == 0 && a[2] == 0 && a[3] == 0;
+    return a[0][0] == 0 && a[0][1] == 0 && a[1][0] == 0 && a[1][1] == 0;
 }
 
 function toZero(a){
-    a[0] = a[1] = a[2] = a[3] = 0;
+    a[0][0] = a[0][1] = a[1][0] = a[1][1] = 0;
     return a;
 }
 
 function toMax(a){
-    a[0] = a[2] = -Number.MAX_VALUE;
-    a[1] = a[3] =  Number.MAX_VALUE;
+    a[0][0] = a[1][0] = -Number.MAX_VALUE;
+    a[0][1] = a[1][1] =  Number.MAX_VALUE;
 }
 
 function createFromPoints(points){
