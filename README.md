52c4dc1e9fbd2a2d261b3cd13b3f1c4bceee10e3
Nick Nikolov <nick@nklv.me>
2017-11-06 12:34:03 +0000

    Update and document aabb.js

--- README.md
+++ README.md
@@ -1,3 +1,83 @@
 # pex-geom
 
 Geometry related code for the pex library
+
+## aabb
+
+`var aabb = require('pex-geom/aabb')`
+
+### `aabb.create()`
+Creates a new axis-aligned bounding box.
+```
+aabb.create()
+// => [[Infinity, Infinity, Infinity], [-Infinity, -Infinity, -Infinity]]
+```
+Returns a new `aabb`.
+
+### `aabb.set(a, b)`
+Sets a bounding box to another.
+
+- `a`: aabb
+- `b`: aabb
+
+Returns `a`.
+
+### `aabb.copy(a)`
+Copies a bounding box.
+
+- `a`: aabb
+
+Returns a new instance of `a`.
+
+### `aabb.copy(a)`
+Copies a bounding box.
+
+- `a`: aabb
+
+Returns a new instance of `a`.
+
+### `aabb.fromPoints(a)`
+Creates a bounding box a list of points.
+
+- `a`: [vec3]
+
+Returns a new `aabb`.
+
+### `aabb.center(a, out)`
+Returns the center of a bounding box.
+
+- `a`: aabb
+- `out`: vec3
+
+Returns `out` or a new `vec3` if `out` isn't passed.
+
+### `aabb.size(a, out)`
+Returns the size of a bounding box.
+
+- `a`: aabb
+- `out`: vec3
+
+Returns `out` or a new `vec3` if `out` isn't passed.
+
+### `aabb.isEmpty(a)`
+Checks if a bounding box is empty.
+
+- `a`: aabb
+
+Returns a `bool`.
+
+### `aabb.includeAABB(a, b)`
+Includes a bounding box in another.
+
+- `a`: aabb
+- `b`: aabb
+
+Returns `a`.
+
+### `aabb.includePoint(a, p)`
+Includes a point in a bounding box.
+
+- `a`: aabb
+- `p`: vec3
+
+Returns `a`.
--- aabb.js
+++ aabb.js
@@ -1,148 +1,102 @@
-function create() {
-    var min = [Infinity, Infinity, Infinity];
-    var max = [-Infinity, -Infinity, -Infinity];
-    return [min, max];
+function create () {
+  var min = [Infinity, Infinity, Infinity]
+  var max = [-Infinity, -Infinity, -Infinity]
+  return [min, max]
 }
 
-function set(a, b) {
-    a[0][0] = b[0][0];
-    a[0][1] = b[0][1];
-    a[0][2] = b[0][2];
-    a[1][0] = b[1][0];
-    a[1][1] = b[1][1];
-    a[1][2] = b[1][2];
+function set (a, b) {
+  a[0][0] = b[0][0]
+  a[0][1] = b[0][1]
+  a[0][2] = b[0][2]
+  a[1][0] = b[1][0]
+  a[1][1] = b[1][1]
+  a[1][2] = b[1][2]
 }
 
-function copy(b) {
-    var a = create();
-    set(a, b);
-    return a;
+function copy (b) {
+  var a = create()
+  set(a, b)
+  return a
 }
 
-function fromPoints(points) {
-    var aabb = create();
-    var min = aabb[0];
-    var max = aabb[1];
+function fromPoints (points) {
+  var aabb = create()
+  var min = aabb[0]
+  var max = aabb[1]
 
-    for(var i=0, len=points.length; i<len; i++) {
-        var p = points[i];
-        min[0] = Math.min(min[0], p[0]);
-        min[1] = Math.min(min[1], p[1]);
-        min[2] = Math.min(min[2], p[2]);
-        max[0] = Math.max(max[0], p[0]);
-        max[1] = Math.max(max[1], p[1]);
-        max[2] = Math.max(max[2], p[2]);
-    }
+  for (var i = 0, len = points.length; i < len; i++) {
+    var p = points[i]
+    min[0] = Math.min(min[0], p[0])
+    min[1] = Math.min(min[1], p[1])
+    min[2] = Math.min(min[2], p[2])
+    max[0] = Math.max(max[0], p[0])
+    max[1] = Math.max(max[1], p[1])
+    max[2] = Math.max(max[2], p[2])
+  }
 
-    return aabb;
+  return aabb
 }
 
-function center(aabb, out) {
-    if(out === undefined){
-        out = [0, 0, 0];
-    }
-    out[0] = (aabb[0][0] + aabb[1][0])/2;
-    out[1] = (aabb[0][1] + aabb[1][1])/2;
-    out[2] = (aabb[0][2] + aabb[1][2])/2;
-    return out;
+function center (aabb, out) {
+  if (out === undefined) {
+    out = [0, 0, 0]
+  }
+  out[0] = (aabb[0][0] + aabb[1][0]) / 2
+  out[1] = (aabb[0][1] + aabb[1][1]) / 2
+  out[2] = (aabb[0][2] + aabb[1][2]) / 2
+  return out
 }
 
-function size(aabb, out) {
-    if(out === undefined){
-        out = [0, 0, 0];
-    }
-    out[0] = Math.abs(aabb[1][0] - aabb[0][0]);
-    out[1] = Math.abs(aabb[1][1] - aabb[0][1]);
-    out[2] = Math.abs(aabb[1][2] - aabb[0][2]);
-    return out;
+function size (aabb, out) {
+  if (out === undefined) {
+    out = [0, 0, 0]
+  }
+  out[0] = Math.abs(aabb[1][0] - aabb[0][0])
+  out[1] = Math.abs(aabb[1][1] - aabb[0][1])
+  out[2] = Math.abs(aabb[1][2] - aabb[0][2])
+  return out
 }
 
-function isEmpty(aabb) {
-    return (aabb[0][0] > aabb[1][0]) || (aabb[0][1] > aabb[1][1]) || (aabb[0][2] > aabb[1][2]);
+function isEmpty (aabb) {
+  return (aabb[0][0] > aabb[1][0]) || (aabb[0][1] > aabb[1][1]) || (aabb[0][2] > aabb[1][2])
 }
 
-function includeAABB(a, b) {
-    if (isEmpty(a)) {
-        set(a, b);
-    }
-    else if (isEmpty(b)) {
-        //do nothing
-    }
-    else {
-        a[0][0] = Math.min(a[0][0], b[0][0])
-        a[0][1] = Math.min(a[0][1], b[0][1])
-        a[0][2] = Math.min(a[0][2], b[0][2])
-        a[1][0] = Math.max(a[1][0], b[1][0])
-        a[1][1] = Math.max(a[1][1], b[1][1])
-        a[1][2] = Math.max(a[1][2], b[1][2])
-    }
+function includeAABB (a, b) {
+  if (isEmpty(a)) {
+    set(a, b)
+  } else if (isEmpty(b)) {
+        // do nothing
+  } else {
+    a[0][0] = Math.min(a[0][0], b[0][0])
+    a[0][1] = Math.min(a[0][1], b[0][1])
+    a[0][2] = Math.min(a[0][2], b[0][2])
+    a[1][0] = Math.max(a[1][0], b[1][0])
+    a[1][1] = Math.max(a[1][1], b[1][1])
+    a[1][2] = Math.max(a[1][2], b[1][2])
+  }
 
-    return a;
+  return a
 }
 
-function includePoint(a, p) {
-    a[0][0] = Math.min(a[0][0], p[0])
-    a[0][1] = Math.min(a[0][1], p[1])
-    a[0][2] = Math.min(a[0][2], p[2])
-    a[1][0] = Math.max(a[1][0], p[0])
-    a[1][1] = Math.max(a[1][1], p[1])
-    a[1][2] = Math.max(a[1][2], p[2])
-    return a;
+function includePoint (a, p) {
+  a[0][0] = Math.min(a[0][0], p[0])
+  a[0][1] = Math.min(a[0][1], p[1])
+  a[0][2] = Math.min(a[0][2], p[2])
+  a[1][0] = Math.max(a[1][0], p[0])
+  a[1][1] = Math.max(a[1][1], p[1])
+  a[1][2] = Math.max(a[1][2], p[2])
+  return a
 }
 
-/**
- * [AABB description]
- * @type {Object}
- */
-var AABB = {
-    /**
-     * [create description]
-     * @return {[type]} [description]
-     */
-    create: create,
+module.exports = {
+  create: create,
+  set: set,
+  copy: copy,
+  fromPoints: fromPoints,
+  center: center,
+  size: size,
+  isEmpty: isEmpty,
+  includeAABB: includeAABB,
+  includePoint: includePoint
+}
 
-    /**
-     * [set description]
-     * @param {[type]} a [description]
-     * @param {[type]} b [description]
-     */
-    set: set,
-    /**
-     * [copy description]
-     * @type {[type]}
-     */
-    copy: copy,
-    /**
-     * [fromPoints description]
-     * @type {[type]}
-     */
-    fromPoints: fromPoints,
-    /**
-     * [center description]
-     * @type {[type]}
-     */
-    center: center,
-    /**
-     * [size description]
-     * @type {[type]}
-     */
-    size: size,
-    /**
-     * AABB is empty if min > max
-     * @param  {[type]}  aabb [description]
-     * @return {Boolean}      [description]
-     */
-    isEmpty: isEmpty,
-    /**
-     * [includeAABB description]
-     * @type {[type]}
-     */
-    includeAABB: includeAABB,
-    /**
-     * [includePoint description]
-     * @type {[type]}
-     */
-    includePoint: includePoint
-};
-
-module.exports = AABB;
