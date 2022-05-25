f6ac7080c8b7a059286531c398b7ade47c1900dd
Damien Seguin <dmnsgn@gmail.com>
2022-05-25 09:09:29 +0100

    feat: remove getRayIntersection

--- plane.js
+++ plane.js
@@ -35,17 +35,6 @@ export function create() {
 }
 
 /**
- * Set the point of intersection betweeen a plane and a ray if it exists to out.
- * @param {plane} plane
- * @param {ray} ray
- * @param {import("pex-math").vec3} out
- * @returns {number}
- */
-export function getRayIntersection(plane, ray, out) {
-  return hitTestPlane(ray, plane[0], plane[1], out);
-}
-
-/**
  * Returns on which side a point is.
  * @param {plane} plane
  * @param {import("pex-math").vec3} point
--- test/plane.js
+++ test/plane.js
@@ -9,40 +9,6 @@ describe("plane", () => {
     deepEqual(plane.create(), [ORIGIN, [0, 1, 0]]);
   });
 
-  describe("getRayIntersection()", () => {
-    it("should find the intersection of the Y up plane and Y up ray at 0, 0, 0", () => {
-      const point = [];
-      plane.getRayIntersection(Y_UP_PLANE, [ORIGIN, [0, 1, 0]], point);
-      deepEqual(point, ORIGIN);
-    });
-    it("should find the intersection of the Y up plane and a negative linear ray at 0, 0, 0", () => {
-      const point = [];
-      plane.getRayIntersection(Y_UP_PLANE, [ORIGIN, [-1, -1, -1]], point);
-      deepEqual(point, ORIGIN);
-    });
-    it("should return 1 if the plane and ray intersect", () => {
-      deepEqual(plane.getRayIntersection(Y_UP_PLANE, [ORIGIN, [0, 1, 0]]), 1);
-    });
-    it("should return -1 if the plane and ray don't intersect (ray on the plane)", () => {
-      deepEqual(
-        plane.getRayIntersection(Y_UP_PLANE, [
-          [0, 0, 1],
-          [0, 0, 2],
-        ]),
-        -1
-      );
-    });
-    it("should return -2 if the plane and ray don't intersect (ray parallel to plane)", () => {
-      deepEqual(
-        plane.getRayIntersection(Y_UP_PLANE, [
-          [0, 1, 1],
-          [0, 1, 2],
-        ]),
-        -2
-      );
-    });
-  });
-
   describe("side()", () => {
     it("should return -1 if the point is on the side of the normal", () => {
       deepEqual(plane.side(Y_UP_PLANE, [0, 1, 0]), plane.SIDE.SAME);
