b677d21bb5f71a5afb892b39c0556f018e0efe3b
Damien Seguin <dmnsgn@gmail.com>
2023-05-10 15:55:38 +0100

    test: add benchmark for aabb.fromPoints

---
+++ benchmark/index.js
@@ -0,0 +1,168 @@
+import { describe, it } from "node:test";
+import { aabb } from "../index.js";
+
+const run = (cb, iterations = 1e6) => {
+  for (let i = 0; i < iterations; i++) {
+    cb();
+  }
+};
+
+const NORM_POINTS = Object.freeze([
+  [-1, -1, -1],
+  [1, -1, -1],
+  [1, -1, 1],
+  [-1, -1, 1],
+  [-1, 1, -1],
+  [1, 1, -1],
+  [1, 1, 1],
+  [-1, 1, 1],
+]);
+const NORM_POINTS_U16 = new Uint16Array(NORM_POINTS.flat());
+const NORM_POINTS_F32 = new Float32Array(NORM_POINTS.flat());
+const MEDIUM_RANDOM_F32 = Float32Array.from({ length: 1_000 }, () =>
+  Math.random()
+);
+const LARGE_RANDOM_F32 = Float32Array.from({ length: 100_000 }, () =>
+  Math.random()
+);
+
+// Fixtures
+function includePointSlice(a, p) {
+  a[0][0] = Math.min(a[0][0], p[0]);
+  a[0][1] = Math.min(a[0][1], p[1]);
+  a[0][2] = Math.min(a[0][2], p[2]);
+  a[1][0] = Math.max(a[1][0], p[0]);
+  a[1][1] = Math.max(a[1][1], p[1]);
+  a[1][2] = Math.max(a[1][2], p[2]);
+  return a;
+}
+
+function fromPointsSlice(a, points) {
+  const isTypedArray = !Array.isArray(points);
+  for (let i = 0; i < points.length / (isTypedArray ? 3 : 1); i++) {
+    includePointSlice(a, isTypedArray ? points.slice(i * 3) : points[i]);
+  }
+
+  return a;
+}
+
+function includePointOffset(a, p, i = 0) {
+  a[0][0] = Math.min(a[0][0], p[i + 0]);
+  a[0][1] = Math.min(a[0][1], p[i + 1]);
+  a[0][2] = Math.min(a[0][2], p[i + 2]);
+  a[1][0] = Math.max(a[1][0], p[i + 0]);
+  a[1][1] = Math.max(a[1][1], p[i + 1]);
+  a[1][2] = Math.max(a[1][2], p[i + 2]);
+  return a;
+}
+function fromPointsOffset(a, points) {
+  const isTypedArray = !Array.isArray(points);
+  for (let i = 0; i < points.length / (isTypedArray ? 3 : 1); i++) {
+    if (isTypedArray) {
+      includePointOffset(a, points, i * 3);
+    } else {
+      includePointOffset(a, points[i]);
+    }
+  }
+
+  return a;
+}
+
+function fromPointsInline(a, points) {
+  const isTypedArray = !Array.isArray(points);
+  if (isTypedArray) {
+    for (let i = 0; i < points.length; i += 3) {
+      a[0][0] = Math.min(a[0][0], points[i + 0]);
+      a[0][1] = Math.min(a[0][1], points[i + 1]);
+      a[0][2] = Math.min(a[0][2], points[i + 2]);
+      a[1][0] = Math.max(a[1][0], points[i + 0]);
+      a[1][1] = Math.max(a[1][1], points[i + 1]);
+      a[1][2] = Math.max(a[1][2], points[i + 2]);
+    }
+  } else {
+    for (let i = 0; i < points.length; i++) {
+      const p = points[i];
+      a[0][0] = Math.min(a[0][0], p[0]);
+      a[0][1] = Math.min(a[0][1], p[1]);
+      a[0][2] = Math.min(a[0][2], p[2]);
+      a[1][0] = Math.max(a[1][0], p[0]);
+      a[1][1] = Math.max(a[1][1], p[1]);
+      a[1][2] = Math.max(a[1][2], p[2]);
+    }
+  }
+  return a;
+}
+
+// Bench
+describe("aabb.fromPoints() compare with array of vec3", () => {
+  it("aabb.fromPoints() using includePoint with offset", () => {
+    const a = aabb.create();
+    run(() => fromPointsOffset(a, NORM_POINTS));
+  });
+  it("aabb.fromPoints() using inline", () => {
+    const a = aabb.create();
+    run(() => fromPointsInline(a, NORM_POINTS));
+  });
+  it("aabb.fromPoints() using slicing for array with offset", () => {
+    const a = aabb.create();
+    run(() => fromPointsSlice(a, NORM_POINTS));
+  });
+});
+
+describe("aabb.fromPoints() compare with Uint16Array", () => {
+  it("aabb.fromPoints() using includePoint with offset", () => {
+    const a = aabb.create();
+    run(() => fromPointsOffset(a, NORM_POINTS_U16));
+  });
+  it("aabb.fromPoints() using inline", () => {
+    const a = aabb.create();
+    run(() => fromPointsInline(a, NORM_POINTS_U16));
+  });
+  it("aabb.fromPoints() using slicing for array with offset", () => {
+    const a = aabb.create();
+    run(() => fromPointsSlice(a, NORM_POINTS_U16));
+  });
+});
+describe("aabb.fromPoints() compare with Float32Array", () => {
+  it("aabb.fromPoints() using includePoint with offset", () => {
+    const a = aabb.create();
+    run(() => fromPointsOffset(a, NORM_POINTS_F32));
+  });
+  it("aabb.fromPoints() using inline", () => {
+    const a = aabb.create();
+    run(() => fromPointsInline(a, NORM_POINTS_F32));
+  });
+  it("aabb.fromPoints() using slicing for array with offset", () => {
+    const a = aabb.create();
+    run(() => fromPointsSlice(a, NORM_POINTS_F32));
+  });
+});
+describe("aabb.fromPoints() compare with medium Float32Array", () => {
+  it("aabb.fromPoints() using includePoint with offset", () => {
+    const a = aabb.create();
+    run(() => fromPointsOffset(a, MEDIUM_RANDOM_F32), 100);
+  });
+  it("aabb.fromPoints() using inline", () => {
+    const a = aabb.create();
+    run(() => fromPointsInline(a, MEDIUM_RANDOM_F32), 100);
+  });
+  it("aabb.fromPoints() using slicing for array with offset", () => {
+    const a = aabb.create();
+    run(() => fromPointsSlice(a, MEDIUM_RANDOM_F32), 100);
+  });
+});
+describe("aabb.fromPoints() compare with large Float32Array", () => {
+  it("aabb.fromPoints() using includePoint with offset", () => {
+    const a = aabb.create();
+    run(() => fromPointsOffset(a, LARGE_RANDOM_F32), 100);
+  });
+  it("aabb.fromPoints() using inline", () => {
+    const a = aabb.create();
+    run(() => fromPointsInline(a, LARGE_RANDOM_F32), 100);
+  });
+  // Too slow
+  // it("aabb.fromPoints() using slicing for array with offset", () => {
+  //   const a = aabb.create();
+  //   run(() => fromPointsSlice(a, LARGE_RANDOM_F32), 10);
+  // });
+});
---
+++ benchmark/utils.js
@@ -0,0 +1,38 @@
+const symbols = {
+  "test:fail": "\u2716 ",
+  "test:pass": "\u2714 ",
+  "arrow:right": "\u25B6 ",
+};
+
+const colors = {
+  green: "\u001b[32m",
+  white: "\u001b[39m",
+  red: "\u001b[31m",
+  gray: "\u001b[90m",
+};
+
+const describe = (name, cb) => {
+  requestAnimationFrame(() => {
+    const label = `${symbols["arrow:right"]} ${name}`;
+    console.log(label);
+    console.time(label);
+    cb();
+    console.timeEnd(label);
+  });
+};
+const it = (name, cb) => {
+  const start = globalThis.performance.now();
+  let prefix = `${colors.green}${symbols["test:pass"]}`;
+  try {
+    cb();
+  } catch (error) {
+    console.error(error);
+    prefix = `${colors.red}${symbols["test:fail"]}`;
+  }
+  console.log(
+    `  ${prefix} ${name} ${colors.gray}(${(
+      globalThis.performance.now() - start
+    ).toFixed(6)}ms)${colors.white}`
+  );
+};
+export { describe, it };
